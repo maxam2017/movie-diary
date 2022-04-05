95792cc84dbc94b7642356e405016827d20ea40a
maxam2017 <whk0313@gmail.com>
2022-04-05 13:31:23 +0800

    feat: setup api proxy

---
+++ src/routes/api/tmdb/[...slug].ts
@@ -0,0 +1,26 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+const TMDB_ENDPOINT = 'https://api.themoviedb.org/3';
+
+export const get: RequestHandler = async ({ params, url, headers }) => {
+  const searchParams = url.searchParams;
+  searchParams.set('api_key', import.meta.env.VITE_TMDB_API_KEY + '');
+  if (!searchParams.get('language')) searchParams.set('language', 'zh-TW');
+  const search = searchParams.toString();
+  const input = TMDB_ENDPOINT + `/${params.slug}` + (search ? `?${search}` : '');
+
+  try {
+    const res = await fetch(input);
+    const json = await res.json();
+    if (res.ok) return { body: json };
+    return {
+      status: res.status,
+      body: { code: res.status, message: json.message },
+    };
+  } catch (err) {
+    return {
+      status: 500,
+      body: { code: 500, message: `internal server error: ${err.message}` },
+    };
+  }
+};
