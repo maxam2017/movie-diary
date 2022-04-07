ffbdf1b57fdda122d1b9ca93d47105021671b6ff
maxam2017 <whk0313@gmail.com>
2022-04-08 01:14:37 +0800

    chore: stylish layout

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,33 @@
+<footer>
+  <div>
+    🍿️ powered by
+    <img
+      alt="tmdb logo"
+      src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg"
+    />
+  </div>
+</footer>
+
+<style lang="scss">
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    width: 100%;
+    background: rgb(var(--tmdbDarkBlue));
+    height: 120px;
+    color: rgba(var(--white), 0.8);
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    height: 60px;
+  }
+
+  img {
+    width: 140px;
+    margin-left: 12px;
+    opacity: 0.8;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,25 @@
+<script context="module">
+  import '$lib/i18n';
+  import { waitLocale } from 'svelte-i18n';
+
+  export async function preload() {
+    return waitLocale();
+  }
+
+  export async function load() {
+    await getConfig();
+    return { status: 200 };
+  }
+</script>
+
 <script>
-  import Header from '$lib/components/header.svelte';
   import Snackbar from '$lib/components/snackbar.svelte';
 
   import '../app.css';
+  import { getConfig } from '$lib/stores/config';
 </script>
 
 <div class="flex-1 flex flex-col">
-  <Header />
   <slot />
   <Snackbar />
 </div>
-
-<style lang="postcss">
-</style>
